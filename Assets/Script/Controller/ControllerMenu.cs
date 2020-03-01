36713b268be05fab252e101bb03c972bbbac8a6f
MatD3mons <matthieu.desmarescaux@isen.yncrea.fr>
2020-03-01 20:54:22 +0100

    Menu c'est dure

---
+++ Assets/Script/Controller/ControllerMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class ControllerMenu : MonoBehaviour
+{
+
+    private List<Person> listofperson = new List<Person>();
+    private static Person personInGame;
+    [SerializeField] String scene;
+
+    [SerializeField] TextMeshProUGUI loginName;
+    [SerializeField] TextMeshProUGUI loginPassword;
+
+    [SerializeField] TextMeshProUGUI RegisterName;
+    [SerializeField] TextMeshProUGUI RegisterPassword;
+    [SerializeField] TextMeshProUGUI RegisterNom;
+    [SerializeField] TextMeshProUGUI RegisterPrenom;
+    [SerializeField] TextMeshProUGUI RegisterEmail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listofperson.Add(new Person("admin", "admin", "admin", "admin", "admin"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Login()
+    {
+        foreach (Person p in listofperson){
+            string pseudo = "admin";
+            string password = "admin";
+
+            Debug.Log(pseudo.Equals(p.getPseudo()));
+            Debug.Log(password.Equals(p.getPassword()));
+
+            if (pseudo.Equals(p.getPseudo()) & password.Equals(p.getPassword()))
+            {
+                personInGame = p;
+                SceneManager.LoadScene(scene);
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+    }
+
+    public void Register()
+    {
+
+    }
+
+}
---
+++ Assets/Script/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person 
+{
+
+    private string pseudo;
+    private string password;
+    private string Nom;
+    private string Prenom;
+    private string Email;
+
+    public Person(string pseudo,string password,string Nom, string Prenom, string Email)
+    {
+        this.pseudo = pseudo;
+        this.password = password;
+        this.Nom = Nom;
+        this.Prenom = Prenom;
+        this.Email = Email;
+    }
+
+    public string getPseudo()
+    {
+        return pseudo;
+    }
+
+    public string getPassword()
+    {
+        return password;
+    }
+}
